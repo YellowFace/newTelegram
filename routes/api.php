4ffb5a909b5ef0e9766a38ae368f535ccb0a1ce8
Andrey Malishev <cfire.vostok@mail.ru>
2021-10-21 01:37:41 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
+Route::get('/webhook', [\App\Http\Controllers\IndexController::class, 'webhook']);
